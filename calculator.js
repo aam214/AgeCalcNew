bfaf06ef49a5e27892174632a3cb5fd9d4ebd038
Ariana <152944814+aam214@users.noreply.github.com>
2024-11-30 21:10:33 -0600

    HTML, CSS, and JS file added. Changed input type to date. Target button and birthDate with getElementById. Added userBirthday function to get the value entered by the user in the input or to show an alert if there is no user input. Added function to calculate birthday by subtracting currentDate with the year of birth. Used click event listener for the button.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,21 @@
 const buttonEnter = document.getElementById("button-enter");
+const birthDate = document.getElementById("date-of-birth");
 
-buttonEnter.addEventListener("click", () => {
-console.log("clicked");
-});
+function userBirthday() {
+const ageValue = birthDate.value;
+if (ageValue === ""){
+  alert("Please enter date of birth 🎂")
+} else {
+  const userAge = birthdayCalculation(ageValue);
+}
+}
+
+function birthdayCalculation(ageValue) {
+const currentDate = new Date();
+const birthYear = new Date(ageValue);
+
+const userAge = currentDate.getFullYear() - birthYear.getFullYear();
+console.log(userAge);
+}
+
+buttonEnter.addEventListener("click", userBirthday);
